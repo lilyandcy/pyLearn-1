b78611980770194be8674fe0b2de95eccc868172
lilyandcy <myyan_yan@msn.com>
2018-07-26 11:53:39 +0800

    Full guess Game saved in a file.

--- Learn_useFiletoSave/saveResultGuessGame_Learn.py
+++ Learn_useFiletoSave/saveResultGuessGame_Learn.py
@@ -1,7 +1,16 @@
 import random
-
+name = raw_input('Please input your name: ')
 f = open('game.txt')
-score = f.read().split()
+lines = f.readlines()
+f.close()
+
+scores={}
+for l in lines:
+    s=l.split()
+    scores[s[0]]=s[1:]
+score = scores.get(name)
+if score is None:
+    score =[0,0,0]
 game_times = int(score[0])
 min_times = int(score[1])
 total_times = int(score[2])
@@ -9,13 +18,15 @@ if game_times >0:
     avg_times = float(total_times) / game_times
 else:
     avg_times = 0
-print'You have played %d times, minimun %d to guess the right answer, average %.2f to get the right answer' %(game_times,min_times,avg_times)
+print'%s have played %d times, minimun %d to guess the right answer, average %.2f to get the right answer' %(name, game_times,min_times,avg_times)
 
 num = random.randint(1,100)
+times = 0
 print'Guess what I think?'
 bingo = False
 while bingo == False:
     answer = int(raw_input())
+    times = times + 1
     if answer < 0:
         print 'Exit the Game'
         break
@@ -26,3 +37,17 @@ while bingo == False:
     if answer == num:
         print'Bingo'
         bingo = True
+
+if game_times == 0 or times < min_times:
+    min_times = times
+total_times = total_times + times
+game_times = game_times +1
+
+scores[name]= [str(game_times), str(min_times), str(avg_times)]
+result = ''
+for n in scores:
+    line = n+' '+' '.join(scores[n]) + '\n'
+    result = result + line
+f = open('game.txt','w')
+f.write(result)
+f.close()
