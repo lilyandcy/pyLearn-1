acb4e4fdad1dbdab95b93661c86d9cfdf9eaa024
lilyandcy <myyan_yan@msn.com>
2018-08-03 14:17:11 +0800

    numpy learning and practice to move into machine learning world

---
+++ Learn_numpy/numpy2_Learn.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.zeros((3,3))
+print (a)
+
+b = np.ones((4,4))
+print (b)
+
+c = np.full((3,4), 8)
+print (c)
+
+d = np.eye(5)
+print (d)
+
+e = np.random.random((2,2))
+print (e)
